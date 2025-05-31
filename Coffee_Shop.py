cd194e939698e1d03e2870ca008682561a03c49a
tomibzan <thomflowen@gmail.com>
2025-05-31 22:04:24 +0300

    nightly progress

--- Coffee_Shop.py
+++ Coffee_Shop.py
@@ -1,30 +1,38 @@
-drinks = ["chocolate", "coffee", "decaf"]
-flavors = ["caramel", "vanilla", "peppermint", "raspberry", "plain"]
-toppings = ["chocolate", "cinnamon", "caramel"]
+
+# Welcome message
 print("Tom's Coffee Shop Drinks")
 print("------------------------")
+
+# Drink selection
 i = 1
 for d in drinks:
     print(i, d)
-    i = i + 1
-drink = input("Choose your drink: ")
-print("Tom's Coffee Shop Flavors")
+    i += 1
+drink_choice = input("Choose your drink: ")
+
+print("\nTom's Coffee Shop Flavors")
 print("------------------------")
 
-i = 1 
+# Flavor selection
+i = 1
 for f in flavors:
     print(i, f)
-    i = i + 1
-flavor = input("Choose your flavor: ")
-print("Tom's Coffee Shop Toppings")
+    i += 1
+flavor_choice = input("Choose your flavor: ")
+
+print("\nTom's Coffee Shop Toppings")
 print("--------------------------")
-i = 1 
+
+# Topping selection
+i = 1
 for t in toppings:
     print(i, t)
-    i = i + 1
-toppings = input("Choose your toppings: ")
-print("Here is your order: ")
-print("Main product: ", drinks [drink])
-print("Flavor: ", flavors[flavor])
-print("Toppings: ", toppings[topping])
-print("Thanks for your order")
+    i += 1
+topping_choice = input("Choose your topping: ")
+
+# Confirm order
+print("\nHere is your order:")
+print("Main product:", drinks[int(drink_choice) - 1])
+print("Flavor:", flavors[int(flavor_choice) - 1])
+print("Topping:", toppings[int(topping_choice) - 1])
+print("Thanks for your order!")
